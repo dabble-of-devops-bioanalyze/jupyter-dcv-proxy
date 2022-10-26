9e8ac3645dbf64519c324b1a0c6c1657c214af36
Jillian Rowe <jillian.e.rowe@gmail.com>
2022-10-26 10:53:39 +0300

    reformat

--- jupyter_dcv_proxy/__init__.py
+++ jupyter_dcv_proxy/__init__.py
@@ -9,18 +9,15 @@ def get_dcv_executable(prog):
     if shutil.which(prog):
         return prog
 
-    raise FileNotFoundError(f'Could not find {prog} in PATH')
+    raise FileNotFoundError(f"Could not find {prog} in PATH")
 
 
 def get_icon_path():
-    return os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'icons', 'dcv.svg'
-    )
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "icons", "dcv.svg")
 
 
 def rewrite_auth(response, request):
-    '''
-    '''
+    """ """
     return
 
 
@@ -28,30 +25,23 @@ def get_system_user():
     try:
         user = pwd.getpwuid(os.getuid())[0]
     except:
-        user = os.environ.get('NB_USER', getpass.getuser())
+        user = os.environ.get("NB_USER", getpass.getuser())
     return user
 
 
 def setup_dcv():
     def _get_env(port):
-        return dict(
-            PORT=port,
-            NB_USER=get_system_user(),
-            SESSION_ID=str(uuid.uuid4())
-        )
+        return dict(PORT=port, NB_USER=get_system_user(), SESSION_ID=str(uuid.uuid4()))
 
     def _get_cmd(port):
-        dcv_exec = get_dcv_executable('dcv')
-        configurable_http_proxy_command = get_dcv_executable('configurable-http-proxy')
+        dcv_exec = get_dcv_executable("dcv")
+        configurable_http_proxy_command = get_dcv_executable("configurable-http-proxy")
         this_dir = os.path.abspath(os.path.dirname(__file__))
         command = f"""bash {this_dir}/run-dcv-user-session.sh"""
         return command.split(" ")
 
     return {
-        'command': _get_cmd,
-        'environment': _get_env,
-        'launcher_entry': {
-            'title': 'DCV',
-            'icon_path': get_icon_path()
-        }
+        "command": _get_cmd,
+        "environment": _get_env,
+        "launcher_entry": {"title": "DCV", "icon_path": get_icon_path()},
     }
--- setup.py
+++ setup.py
@@ -2,23 +2,17 @@ import setuptools
 
 setuptools.setup(
     name="jupyter-dcv-proxy",
-    version='2.0.2',
+    version="2.0.2",
     url="https://github.com/dabble-of-devops-bioanalyze/jupyter-dcv-proxy",
     author="Jillian Rowe",
     description="Jupyter extension to proxy DCV",
     packages=setuptools.find_packages(),
-    keywords=['Jupyter'],
-    classifiers=['Framework :: Jupyter'],
-    scripts=['jupyter_dcv_proxy/run-dcv-user-session.sh'],
-    install_requires=[
-        'jupyter-server-proxy>=3.2.0'
-    ],
-    entry_points={
-        'jupyter_serverproxy_servers': [
-            'dcv = jupyter_dcv_proxy:setup_dcv'
-        ]
-    },
+    keywords=["Jupyter"],
+    classifiers=["Framework :: Jupyter"],
+    scripts=["jupyter_dcv_proxy/run-dcv-user-session.sh"],
+    install_requires=["jupyter-server-proxy>=3.2.0"],
+    entry_points={"jupyter_serverproxy_servers": ["dcv = jupyter_dcv_proxy:setup_dcv"]},
     package_data={
-        'jupyter_dcv_proxy': ['icons/dcv.svg'],
+        "jupyter_dcv_proxy": ["icons/dcv.svg"],
     },
 )
