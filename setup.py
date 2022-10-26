cba72a51c9ca22ee8c3aa0de21a53ec08ad443d0
Jillian Rowe <jillian.e.rowe@gmail.com>
2022-10-26 14:23:09 +0300

    adding some packagesf

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 import setuptools
 import versioneer
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="jupyter-dcv-proxy",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     url="https://github.com/dabble-of-devops-bioanalyze/jupyter-dcv-proxy",
     author="Jillian Rowe",
     description="Jupyter extension to proxy DCV",
+    license="Apache Software License 2.0",
+    long_description=long_description,
     packages=setuptools.find_packages(),
     keywords=["Jupyter"],
     classifiers=["Framework :: Jupyter"],
@@ -18,4 +21,6 @@ setuptools.setup(
     package_data={
         "jupyter_dcv_proxy": ["icons/dcv.svg"],
     },
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
